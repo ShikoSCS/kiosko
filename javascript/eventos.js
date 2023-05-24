852ea35d96791a6cd2644d54f1423bc257a9cfca
ShikoSCS <shikoscs@gmail.com>
2023-05-24 18:20:29 -0300

    Añadiendo un pequeño script para hacer una galeria

--- javascript/eventos.js
+++ javascript/eventos.js
@@ -1,1 +1,12 @@
-
+document.addEventListener("DOMContentLoaded", function(event) {
+    var galleryItems = document.querySelectorAll(".gallery-item");
+    var currentImageIndex = 0;
+  
+    setInterval(function() {
+      galleryItems[currentImageIndex].classList.remove("hidden");
+      galleryItems[currentImageIndex].classList.add("hidden");
+      currentImageIndex = (currentImageIndex + 1) % galleryItems.length;
+      galleryItems[currentImageIndex].classList.remove("hidden");
+    }, 3000);
+  });
+  
